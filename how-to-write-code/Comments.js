e604614403ab9726cb14e7a9b94caeb0874e0423
Alyssa Marsala <marsala@Alyssas-Air.lan>
2023-08-20 22:09:41 -0500

    <assign2>

--- how-to-write-code/Comments.js
+++ how-to-write-code/Comments.js
@@ -2,11 +2,12 @@
 
    // console.log("This does not print.");
 
-   console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
+   console.log("Hell0!"); // Comments do not have to start at the beginning of a line.
 
    /* Here is how
-   to have
    multi-line
    comments. */
+   /* I am adding in my own comments. */
+   // anything with slashs will not print 
 
-   console.log("Comments make your code more readable by others.");
+   console.log("This is printing something that should not be a comment.");
